0e6bca64d3d94d4e4a67006d50f9ebf2751716df
crabbey <crabbey@crabbey.co.uk>
2020-12-06 21:22:44 +0000

    Added puzzle 5 and 6

--- puzzle5/puzzle5.go
+++ puzzle5/puzzle5.go
@@ -1,24 +1,68 @@
 package puzzle5
 
 import (
-	
+	"strings"
+	"math"
+	"fmt"
 	"github.com/crabbey/aoc2020/common"
 	"github.com/davecgh/go-spew/spew"
 )
 
 var _ = spew.Dump
+var _ = fmt.Println
 
 type Puzzle5 struct {
 
+}
+
+type Seat struct {
+	Row int
+	Col int
+	Uid int
+}
+
+func (p Puzzle5) LoadSeats(input []string) []Seat {
+	var ret []Seat
+	rowMax := 127
+	colMax := 7
+	for _, line := range input {
+		seat := Seat{}
+		char := strings.Split(line, "")
+		for x := 0; x < 7; x++ {
+			if string(char[x]) == "B" {
+				plusone := float64(x + 1)
+				seat.Row += ((rowMax + 1) / int(math.Pow(2, plusone))) 
+			}
+			continue
+		}
+		for x := 7; x < 10; x++ {
+			if string(char[x]) == "R" {
+				// -6 because we start at 7
+				plusone := float64(x - 6)
+				seat.Col += ((colMax + 1) / int(math.Pow(2, plusone))) 
+			}
+			continue
+		}
+		seat.Uid = seat.Row * 8 + seat.Col
+		ret = append(ret, seat)
+	}
+	return ret
 }
 
 func (p Puzzle5) Part1(input common.AoCInput, output *common.AoCSolution) (*common.AoCSolution, error) {
 	i, err := input.Read()
 	if err != nil {
 		spew.Dump(i)
-	return output, err
+		return output, err
 	}
-	spew.Dump(i)
+	seats := p.LoadSeats(i)
+	highest := 0
+	for _, seat := range seats {
+		if seat.Uid > highest {
+			highest = seat.Uid
+		}
+	}
+	output.Text = fmt.Sprintf("Seat UID %v is the highest", highest)
 	return output, nil
 }
 
@@ -26,8 +70,24 @@ func (p Puzzle5) Part2(input common.AoCInput, output *common.AoCSolution) (*comm
 	i, err := input.Read()
 	if err != nil {
 		spew.Dump(i)
-	return output, err
+		return output, err
 	}
-	spew.Dump(i)
+
+	seats := p.LoadSeats(i)
+	var seatUids []int
+	seatUidMap := make(map[int]Seat)
+	for _, seat := range seats {
+		seatUids = append(seatUids, seat.Uid)
+		seatUidMap[seat.Uid] = seat
+	}
+	for _, uid := range seatUids {
+		// If the next one doesn't exist (is vacant) but the one after that exists, it's yours
+		if _, ok := seatUidMap[uid + 1] ; !ok {
+			if _, ok2 := seatUidMap[uid + 2] ; ok2 {
+				output.Text = fmt.Sprintf("Seat UID %v is yours", uid+1)
+				return output, nil
+			}
+		}
+	}
 	return output, nil
 }
--- puzzle6/puzzle6.go
+++ puzzle6/puzzle6.go
@@ -1,7 +1,8 @@
 package puzzle6
 
 import (
-	
+	"strings"
+	"fmt"
 	"github.com/crabbey/aoc2020/common"
 	"github.com/davecgh/go-spew/spew"
 )
@@ -12,13 +13,48 @@ type Puzzle6 struct {
 
 }
 
+type Group struct {
+	Answers map[string]int
+	NumPeople int
+}
+
+func (p Puzzle6) LoadResponses(input []string) []Group {
+	g := Group{
+		Answers: make(map[string]int),
+	}
+	var ret []Group
+	for _, line := range input {
+		if line == "" {
+			ret = append(ret, g)
+			g = Group{
+				Answers: make(map[string]int),
+			}
+			continue
+		}
+		g.NumPeople++
+		chars := strings.Split(line, "")
+		for _, char := range chars {
+			g.Answers[char]++
+		}
+	}
+	ret = append(ret, g)
+	return ret
+}
+
 func (p Puzzle6) Part1(input common.AoCInput, output *common.AoCSolution) (*common.AoCSolution, error) {
 	i, err := input.Read()
 	if err != nil {
 		spew.Dump(i)
-	return output, err
+		return output, err
 	}
-	spew.Dump(i)
+	groups := p.LoadResponses(i)
+	count := 0
+	for _, group := range groups {
+		for _, _ = range group.Answers {
+			count++
+		}
+	}
+	output.Text = fmt.Sprintf("Count: %v\n", count)
 	return output, nil
 }
 
@@ -26,8 +62,17 @@ func (p Puzzle6) Part2(input common.AoCInput, output *common.AoCSolution) (*comm
 	i, err := input.Read()
 	if err != nil {
 		spew.Dump(i)
-	return output, err
+		return output, err
 	}
-	spew.Dump(i)
+	groups := p.LoadResponses(i)
+	count := 0
+	for _, group := range groups {
+		for _, total := range group.Answers {
+			if total == group.NumPeople {
+				count++
+			}
+		}
+	}
+	output.Text = fmt.Sprintf("Count: %v\n", count)
 	return output, nil
 }
